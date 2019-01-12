514c585d9fef2533f170321760229f8c8ca94587
Your Name <ilin96inbox@gmail.com>
2019-01-12 19:40:14 +0800

    background sync

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,22 +1,26 @@
-var shareImageButton = document.querySelector("#share-image-button");
-var createPostArea = document.querySelector("#create-post");
-var closeCreatePostModalButton = document.querySelector(
-  "#close-create-post-modal-btn"
-);
-var sharedMomentsArea = document.querySelector("#shared-moments");
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
-  createPostArea.style.display = "block";
+  // createPostArea.style.display = 'block';
+  // setTimeout(function() {
+    createPostArea.style.transform = 'translateY(0)';
+  // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
     deferredPrompt.userChoice.then(function(choiceResult) {
       console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === "dismissed") {
-        console.log("User cancelled installation");
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
       } else {
-        console.log("User added to home screen");
+        console.log('User added to home screen');
       }
     });
 
@@ -34,48 +38,49 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = "none";
+  createPostArea.style.transform = 'translateY(100vh)';
+  // createPostArea.style.display = 'none';
 }
 
-shareImageButton.addEventListener("click", openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log("clicked");
-  if ("caches" in window) {
-    caches.open("user-requested").then(function(cache) {
-      cache.add("https://httpbin.org/get");
-      cache.add("/src/images/sf-boat.jpg");
-    });
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function(cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
   }
 }
 
 function clearCards() {
-  while (sharedMomentsArea.hasChildNodes()) {
+  while(sharedMomentsArea.hasChildNodes()) {
     sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
   }
 }
 
 function createCard(data) {
-  var cardWrapper = document.createElement("div");
-  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
-  var cardTitle = document.createElement("div");
-  cardTitle.className = "mdl-card__title";
-  cardTitle.style.backgroundImage = "url(" + data.image + ")";
-  cardTitle.style.backgroundSize = "cover";
-  cardTitle.style.height = "180px";
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+  cardTitle.style.backgroundSize = 'cover';
   cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement("h2");
-  cardTitleTextElement.style.color = "white";
-  cardTitleTextElement.className = "mdl-card__title-text";
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement("div");
-  cardSupportingText.className = "mdl-card__supporting-text";
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = data.location;
-  cardSupportingText.style.textAlign = "center";
+  cardSupportingText.style.textAlign = 'center';
   // var cardSaveButton = document.createElement('button');
   // cardSaveButton.textContent = 'Save';
   // cardSaveButton.addEventListener('click', onSaveButtonClicked);
@@ -92,7 +97,7 @@ function updateUI(data) {
   }
 }
 
-var url = "https://pwdgram.firebaseio.com/posts.json";
+var url = 'https://pwagram-99adf.firebaseio.com/posts.json';
 var networkDataReceived = false;
 
 fetch(url)
@@ -101,7 +106,7 @@ fetch(url)
   })
   .then(function(data) {
     networkDataReceived = true;
-    console.log("From web", data);
+    console.log('From web', data);
     var dataArray = [];
     for (var key in data) {
       dataArray.push(data[key]);
@@ -109,11 +114,68 @@ fetch(url)
     updateUI(dataArray);
   });
 
-if ("indexedDB" in window) {
-  readAllData("posts").then(function(data) {
-    if (!networkDataReceived) {
-      console.log("From cache", data);
-      updateUI(data);
-    }
-  });
+if ('indexedDB' in window) {
+  readAllData('posts')
+    .then(function(data) {
+      if (!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
 }
+
+function sendData() {
+  fetch('https://us-central1-pwagram-99adf.cloudfunctions.net/storePostData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      id: new Date().toISOString(),
+      title: titleInput.value,
+      location: locationInput.value,
+      image: 'https://firebasestorage.googleapis.com/v0/b/pwagram-99adf.appspot.com/o/sf-boat.jpg?alt=media&token=19f4770c-fc8c-4882-92f1-62000ff06f16'
+    })
+  })
+    .then(function(res) {
+      console.log('Sent data', res);
+      updateUI();
+    })
+}
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (titleInput.value.trim() === '' || locationInput.value.trim() === '') {
+    alert('Please enter valid data!');
+    return;
+  }
+
+  closeCreatePostModal();
+
+  if ('serviceWorker' in navigator && 'SyncManager' in window) {
+    navigator.serviceWorker.ready
+      .then(function(sw) {
+        var post = {
+          id: new Date().toISOString(),
+          title: titleInput.value,
+          location: locationInput.value
+        };
+        writeData('sync-posts', post)
+          .then(function() {
+            return sw.sync.register('sync-new-posts');
+          })
+          .then(function() {
+            var snackbarContainer = document.querySelector('#confirmation-toast');
+            var data = {message: 'Your Post was saved for syncing!'};
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+  } else {
+    sendData();
+  }
+});
